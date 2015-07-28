f8381a57f62bc529321582b3caa17369402efa7d
MrCiprian89 <mr.ciprian89@gmail.com>
2015-07-28 10:46:18 -0400

    created function that allows you to set the maximum amount of characters a row will show in the sidebar and replace the remaining characters with an elipses

--- COMP2007-Final-SteveEd/Scripts/main.js
+++ COMP2007-Final-SteveEd/Scripts/main.js
@@ -1,4 +1,16 @@
-﻿$("#menu-toggle").click(function (e) {
+﻿
+//SIDEBAR add elipses when campaign name is too long
+var MAXCHAR = 12;
+var listItems = $("#sidebar li a");
+listItems.each(function (a) {
+    if ($(this).text().length >= MAXCHAR) {
+        var string = $(this).text().substring(0, MAXCHAR) + "...";
+        $(this).text(string);
+    }
+});
+
+//SIDEBAR toggle it to minimize and maximize
+$("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("active");
 });
