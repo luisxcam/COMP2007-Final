cf4ae877215d2978f94e4790045947cce4f0ceb0
luisxcam <luisxcam@gmail.com>
2015-08-02 17:37:28 -0400

    Updated unique values

--- DB_Querys/BSL_Database_Master.sql
+++ DB_Querys/BSL_Database_Master.sql
@@ -182,11 +182,11 @@ CREATE TABLE [dbo].[Spells]
 CREATE TABLE [dbo].[Users]
 (
 	[UsernameId] INT NOT NULL IDENTITY (1, 1),
-	[Username] VARCHAR(50) NOT NULL,
+	[Username] VARCHAR(50) NOT NULL UNIQUE,
 	[FirstName] VARCHAR(50) NOT NULL,
 	[LastName] VARCHAR(50) NOT NULL,
 	[Password] VARCHAR(128) NOT NULL,
-	[Email] VARCHAR(100) NOT NULL,
+	[Email] VARCHAR(100) NOT NULL UNIQUE,
 	PRIMARY KEY CLUSTERED ([UsernameId] ASC)
 );
 
