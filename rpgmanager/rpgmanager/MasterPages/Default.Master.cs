bf0178c602a3f300718cf2ab3442a59f082bf96e
MrCiprian89 <mr.ciprian89@gmail.com>
2015-08-06 02:41:03 -0400

    added javascript

--- rpgmanager/rpgmanager/MasterPages/Default.Master.cs
+++ rpgmanager/rpgmanager/MasterPages/Default.Master.cs
@@ -4,22 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using rpgmanager.Models;
+using System.Data.Entity;
 
 namespace rpgmanager
 {
     public partial class Default : System.Web.UI.MasterPage
     {
+
+        protected rpgmanager.Models.rpg_entities _db = new rpgmanager.Models.rpg_entities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!HttpContext.Current.User.Identity.IsAuthenticated)
             {
                 PrivateNav.Visible = false;
                 PublicNav.Visible = true;
+                //body.Attributes["class"] = "loggedOut";
             }
             else
             {
                 PrivateNav.Visible = true;
                 PublicNav.Visible = false;
+                //body.Attributes["class"] = "";
+            }
+        }
+        // This is the Insert method to insert the entered Campaign item
+        // USAGE: <asp:FormView InsertMethod="InsertItem">
+        public void InsertItem()
+        {
+            using (_db)
+            {
+                var item = new rpgmanager.Models.Campaign();
+
+                TryUpdateModel(item);
+
+                // Save changes
+                _db.Campaigns.Add(item);
+                _db.SaveChanges();
+
+                Response.Redirect("Default");
+            }
+        }
+
+        protected void ItemCommand(object sender, FormViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("Cancel", StringComparison.OrdinalIgnoreCase))
+            {
+                Response.Redirect("Default");
             }
         }
     }
